ee713fdb092425b91958c61cec2c2050ffbd5d48
Liam G <liam.garriga@tutanota.com>
2022-07-09 09:01:16 +0200

    Added documentation link

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 use std::io::Read;
 
 /// Reads a pipe from stdin and returns it as a `String`.
@@ -12,9 +14,9 @@ pub fn read_pipe() -> Option<String> {
     (!buf.is_empty()).then_some(buf.trim().into())
 }
 
-/// Reads pipe from stdin, splits it at whitespace and returns it as a Vec<String>.
+/// Reads pipe from stdin, splits it by whitespace and returns it as a `Vec<String>`.
 /// 
-/// See [`read_pipe`]() for more information.
+/// See [`read_pipe`](https://docs.rs/read_pipe/0.1.1/read_pipe/fn.read_pipe.html) for more information.
 pub fn read_pipe_split_whitespace() -> Option<Vec<String>> {
     let read = read_pipe()?;
     Some(read.split_whitespace().map(|s| s.into()).collect())
