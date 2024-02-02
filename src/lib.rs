8500a5912f3daecd23235f24c6ccd8efa3f60613
LyonSyonII <liam.garriga@tutanota.com>
2024-02-03 00:21:44 +0100

    removed atty from dependencies and use std::io::IsTerminal instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![doc = include_str!("../README.md")]
 
-use std::io::Read;
+use std::io::{IsTerminal, Read};
 
 /// Reads a pipe from stdin and returns it as a `String`.
 /// # Errors
 /// Will return `None` if there is no pipe handled to the program or some error occurs while reading stdin (see [`std::io::Read::read_to_string`](https://doc.rust-lang.org/std/io/trait.Read.html#method.read_to_string) for more information).
 pub fn read_pipe() -> Option<String> {
     let mut buf = String::new();
-    if atty::isnt(atty::Stream::Stdin) {
+    if !std::io::stdin().is_terminal() {
         std::io::stdin().read_to_string(&mut buf).ok()?;
     }
     
@@ -16,7 +16,7 @@ pub fn read_pipe() -> Option<String> {
 
 /// Reads pipe from stdin, splits it by whitespace and returns it as a `Vec<String>`.
 /// 
-/// See [`read_pipe`](https://docs.rs/read_pipe/0.1.1/read_pipe/fn.read_pipe.html) for more information.
+/// See [`read_pipe`] for more information.
 pub fn read_pipe_split_whitespace() -> Option<Vec<String>> {
     let read = read_pipe()?;
     Some(read.split_whitespace().map(|s| s.into()).collect())
